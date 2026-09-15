47ecc1addd8f4aa607c8dda456041bd665806611
Jeremy Howard <github@jhoward.fastmail.fm>
2026-09-15 15:53:46 +1000

    bump

--- fastcdp/__init__.py
+++ fastcdp/__init__.py
@@ -4,6 +4,6 @@ Modules:
 
 - `fastcdp.skill`: Work with Chrome through the DevTools Protocol: inspect pages, interact with controls, and diagnose browser applications."""
 
-__version__ = "0.0.17"
+__version__ = "0.0.18"
 from .core import *
 from .ext import *
